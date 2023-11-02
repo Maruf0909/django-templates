1e40203af9e1dd9ec9251a467ae9506f9932bc2c
djumanov <diyorbekjumanov1120@gmail.com>
2023-11-02 09:36:14 +0500

    templates

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
+
+class HomeView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'home.html')
