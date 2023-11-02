fce204c9c9018c12b84a7be21efcfc9874ae93af
djumanov <diyorbekjumanov1120@gmail.com>
2023-11-02 10:37:09 +0500

    blog model

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpRequest, HttpResponse
+from .models import Blog
 
 
 class HomeView(View):
@@ -20,4 +21,7 @@ class ContactView(View):
 
 class BlogsView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
-        return render(request, 'blogs.html')
+        content = {
+            'blogs': [{'id': blog.id, 'title': blog.title, 'content': blog.content[:100], 'published': blog.published} for blog in Blog.objects.all()]
+        }
+        return render(request, 'blogs.html', context=content)
