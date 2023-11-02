3f1d245bcc91c09a5b3a79b330580d59333fe20a
djumanov <diyorbekjumanov1120@gmail.com>
2023-11-02 10:03:51 +0500

    all html

--- mysite/views.py
+++ mysite/views.py
@@ -3,7 +3,21 @@ from django.views import View
 from django.http import HttpRequest, HttpResponse
 
 
-
 class HomeView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         return render(request, 'home.html')
+
+
+class AboutView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'about.html')
+
+
+class ContactView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'contact.html')
+
+
+class BlogsView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'blogs.html')
